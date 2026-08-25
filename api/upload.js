359b4ec738a2297b6f8313f367601c84e7263531
kennethoperio <junekennethsiscario@gmail.com>
2026-08-25 23:17:43 +0800

    Clean RSA Key in api/upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,9 +1,6 @@
-const crypto = require('crypto');
-const https = require('https');
-
 const GDRIVE_CREDENTIALS = {
   client_email: "it-portal-storage@fluid-arc-506004-a6.iam.gserviceaccount.com",
-  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDAEYqKK4hdGAFc\nAMhToJJbwXzFfHCzE76dQVDrPxvLnchIvr3odOm/hIhKuTGg7iwU46toTu3RaIJz\nEtC5qFtUDiWoevnP6iSqCtraCdkn0+NwHl0fBie9Kuf7ug4aAB+6EIpOYATdHjWb\n4eQBoNH5Ex6xii/AaUYibbUNIAaqmpFZt+q9UXo0RWsvSzB7zFRWi/PZWAHTfDjD\nXDplmnKMAexVE6gProbDGKWrGRHuf/MlvM6tvTl8Q1NzF1WJZ32pjQ03qdbvnKzD\nwFsQCbXxyv5W6ek89MvwJlHtDQ6c0XcVzzcwHDh3BZJF9Y1mB6holj/XXxoYd5P3\nr9KsDtHFAgMBAAECggEABSrjppSFwnVxIevOd/uHvIq/4+NVd+f11q7Jcc7cnVWV\nLCnfm6e7m0DCVvpVFL6btoMqmy+Wc+4jJlvw/DHEpUYNKtOGMZsb2exZV4jwzALG\nKX/ToxBMFOmY3Lu0gewTbnLf+bxZHSbhK9y/wPB1/cTPLFkqsDtU3PvFJYGBVGkw\ntAzLyOykB0SGeXxpiaMKX/Kqo4Pt1ep8h0c0LDiui9X1dibY2Na3UNIAaqmpFZt+q\n9UXo0RWsvSzB7zFRWi/PZWAHTfDjDXDplmnKMAexVE6gProbDGKWrGRHuf/MlvM6\ntvTl8Q1NzF1WJZ32pjQ03qdbvnKzDwFsQCbXxyv5W6ek89MvwJlHtDQ6c0XcVzzcw\nHDh3BZJF9Y1mB6holj/XXxoYd5P3r9KsDtHFAgMBAAECggEABSrjppSFwnVxIevO\nd/uHvIq/4+NVd+f11q7Jcc7cnVWVLCnfm6e7m0DCVvpVFL6btoMqmy+Wc+4jJlvw\n/DHEpUYNKtOGMZsb2exZV4jwzALGKX/ToxBMFOmY3Lu0gewTbnLf+bxZHSbhK9y/\nwPB1/cTPLFkqsDtU3PvFJYGBVGkwtAzLyOykB0SGeXxpiaMKX/Kqo4Pt1ep8h0c0\nLDiui9X1dibY2Na3ONQj4lQo4888nVUainDAmxR6http8zfDCIiUy+KCreBFs0Bb\n+WUxHqqhjvHGtYQJ1QivLaliRyOgxiS2MiIlKuL8dDPZqRpj/dUanTckgpl8GyJo\n3fsJmQKBgQD3AxVN3zunCuynJPJ5hVnXsgvgDhAOPPLwxtaun4Ky5F90dPRHjVaI\n4WvgWdX5ifCMqmzLh8nGunwJ2teg+kmTEUmW10Aa7Mo3lUlrI1z2AqCLpbFHFwwO\ncPVcNibC2dNtI1EU4nxi+II0MGAi7L01kneEstcFRlDbZpv1F1isnQKBgQDHDqi8\nDPR5oY2mrMEHGTVAt/MEH7J3qFjxMvga8EEWgpM3gWqMJaIQdX6kp62Oy/yzT8Lf\niOzxyHsocYdrSxpAERQMJDBNy3DFwazODyHpu3FpWmMv4Vhf11EpD2sBUGGc8gXe\n0GNIqKGW5byi2WWVr2CwkgBMnCSDaUII3MYtSQKBgBvsxE7Oat8ClCh9O9BTLAn/\nfeoxjM0fRNPFluWc8NiqisQOqMMiDmNhIKH3ZgJU/tXYOn5z9nK6CGXQ0MnJIeI3\ndRtRcFTa6i2IeglbsRm6yE2hSL5hs6I6UPLAyHcEyysub+8tf6RstcOSqHuqSeWx\njkN5OGfHQELdgcoefo7dAoGAGIkYB0XZhHyDRz4X9NYImFeUHrgBeXpIrEJKDpf6\njdm+Z6MODQQ+e6Tf3U/Ftsox9bApJwBrpEm/1HZZ6MGzFJ6GSBDV22DuH5IFyMhY\nt8Sg8AlyHF68U+PoXxVFbT4JKh0y2An7kuMmN8FNhQ0i1lZtppX4b3j3jzMULp93\n1fECgYAi5RilEdekd+qxxl2bLmCTb7KWuPNMmvGq21YeOmhT7iqTJisBnnlfNyy4\nx5MakVswlCi7knSmMXBRtoIB6xfkHwq1slLSUDyYGDsHaBBLzhXszyqaFM5SkeMT\nFDNMngpqZHBfFnz+p378YZJT4m2TSy92OWfpSxgS01wOCKKsUQ==\n-----END PRIVATE KEY-----\n"
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDAEYqKK4hdGAFc\nAMhToJJbwXzFfHCzE76dQVDrPxvLnchIvr3odOm/hIhKuTGg7iwU46toTu3RaIJz\nEtC5qFtUDiWoevnP6iSqCtraCdkn0+NwHl0fBie9Kuf7ug4aAB+6EIpOYATdHjWb\n4eQBoNH5Ex6xii/AaUYibbUNIAaqmpFZt+q9UXo0RWsvSzB7zFRWi/PZWAHTfDjD\nXDplmnKMAexVE6gProbDGKWrGRHuf/MlvM6tvTl8Q1NzF1WJZ32pjQ03qdbvnKzD\nwFsQCbXxyv5W6ek89MvwJlHtDQ6c0XcVzzcwHDh3BZJF9Y1mB6holj/XXxoYd5P3\nr9KsDtHFAgMBAAECggEABSrjppSFwnVxIevOd/uHvIq/4+NVd+f11q7Jcc7cnVWV\nLCnfm6e7m0DCVvpVFL6btoMqmy+Wc+4jJlvw/DHEpUYNKtOGMZsb2exZV4jwzALG\nKX/ToxBMFOmY3Lu0gewTbnLf+bxZHSbhK9y/wPB1/cTPLFkqsDtU3PvFJYGBVGkw\ntAzLyOykB0SGeXxpiaMKX/Kqo4Pt1ep8h0c0LDiui9X1dibY2Na3ONQj4lQo4888\nnVUainDAmxR6http8zfDCIiUy+KCreBFs0Bb+WUxHqqhjvHGtYQJ1QivLaliRyOg\nxiS2MiIlKuL8dDPZqRpj/dUanTckgpl8GyJo3fsJmQKBgQD3AxVN3zunCuynJPJ5\nhVnXsgvgDhAOPPLwxtaun4Ky5F90dPRHjVaI4WvgWdX5ifCMqmzLh8nGunwJ2teg\n+kmTEUmW10Aa7Mo3lUlrI1z2AqCLpbFHFwwOcPVcNibC2dNtI1EU4nxi+II0MGAi\n7L01kneEstcFRlDbZpv1F1isnQKBgQDHDqi8DPR5oY2mrMEHGTVAt/MEH7J3qFjx\nMvga8EEWgpM3gWqMJaIQdX6kp62Oy/yzT8LfiOzxyHsocYdrSxpAERQMJDBNy3DF\nwazODyHpu3FpWmMv4Vhf11EpD2sBUGGc8gXe0GNIqKGW5byi2WWVr2CwkgBMnCSD\naUII3MYtSQKBgBvsxE7Oat8ClCh9O9BTLAn/feoxjM0fRNPFluWc8NiqisQOqMMi\nDmNhIKH3ZgJU/tXYOn5z9nK6CGXQ0MnJIeI3dRtRcFTa6i2IeglbsRm6yE2hSL5h\ns6I6UPLAyHcEyysub+8tf6RstcOSqHuqSeWxjkN5OGfHQELdgcoefo7dAoGAGIkY\nB0XZhHyDRz4X9NYImFeUHrgBeXpIrEJKDpf6jdm+Z6MODQQ+e6Tf3U/Ftsox9bAp\nJwBrpEm/1HZZ6MGzFJ6GSBDV22DuH5IFyMhYt8Sg8AlyHF68U+PoXxVFbT4JKh0y\n2An7kuMmN8FNhQ0i1lZtppX4b3j3jzMULp931fECgYAi5RilEdekd+qxxl2bLmCT\nb7KWuPNMmvGq21YeOmhT7iqTJisBnnlfNyy4x5MakVswlCi7knSmMXBRtoIB6xfk\nHwq1slLSUDyYGDsHaBBLzhXszyqaFM5SkeMTFDNMngpqZHBfFnz+p378YZJT4m2T\nSy92OWfpSxgS01wOCKKsUQ==\n-----END PRIVATE KEY-----\n"
 };
 
 const DEFAULT_PARENT_FOLDER_ID = '15FIr_ZPXyTJUILkgpsvK_sGbmhPj3QJ3';
@@ -28,7 +25,7 @@ function getGoogleAccessToken() {
       iat: now
     }));
 
-    const unsignedToken = `${header}.${claimSet}`;
+    const unsignedToken = ${header}.;
     const signer = crypto.createSign('RSA-SHA256');
     signer.update(unsignedToken);
     const signature = signer.sign(GDRIVE_CREDENTIALS.private_key, 'base64')
@@ -36,8 +33,8 @@ function getGoogleAccessToken() {
       .replace(/\+/g, '-')
       .replace(/\//g, '_');
 
-    const jwt = `${unsignedToken}.${signature}`;
-    const postData = `grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=${jwt}`;
+    const jwt = ${unsignedToken}.;
+    const postData = grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=;
 
     const req = https.request('https://oauth2.googleapis.com/token', {
       method: 'POST',
@@ -74,7 +71,7 @@ async function initResumableSession(token, fileName, parentId, fileSize, mimeTyp
   let response = await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&supportsAllDrives=true', {
     method: 'POST',
     headers: {
-      'Authorization': `Bearer ${token}`,
+      'Authorization': Bearer ,
       'Content-Type': 'application/json; charset=UTF-8',
       'X-Upload-Content-Type': mimeType || 'application/octet-stream'
     },
@@ -87,7 +84,7 @@ async function initResumableSession(token, fileName, parentId, fileSize, mimeTyp
     response = await fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&supportsAllDrives=true', {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${token}`,
+        'Authorization': Bearer ,
         'Content-Type': 'application/json; charset=UTF-8',
         'X-Upload-Content-Type': mimeType || 'application/octet-stream'
       },
@@ -100,7 +97,7 @@ async function initResumableSession(token, fileName, parentId, fileSize, mimeTyp
     return locationUrl;
   }
   const text = await response.text();
-  throw new Error(`Google Drive API returned status ${response.status}: ${text}`);
+  throw new Error(Google Drive API returned status : );
 }
 
 module.exports = async (req, res) => {
