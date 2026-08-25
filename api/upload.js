bf25fe0540ff8f0064d5fea5fc53e4b3999599ff
kennethoperio <junekennethsiscario@gmail.com>
2026-08-25 21:47:27 +0800

    CORS fix & smooth realtime upload progress in api/upload.js

--- api/upload.js
+++ api/upload.js
@@ -114,8 +114,9 @@ function uploadBinaryToGoogleDrive(accessToken, fileName, mimeType, parentFolder
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
